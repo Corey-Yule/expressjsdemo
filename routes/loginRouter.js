3052f1d0fa7af8ccf61c80e05d4f2d74f8f19b68
James <jg2077@hw.ac.uk>
2026-01-04 16:27:13 +0000

    re added the databes test button, a little formatting

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/loginPage', (req, res) => {
+router.get('/', (req, res) => {
   res.render('LoginPage/login')
 })
 
