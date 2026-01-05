858e0a43f799b146e53cf4968674a549ebe3896d
James <jg2077@hw.ac.uk>
2026-01-05 22:43:03 +0000

    login system connected to supabase, can now create accounts and 'login' to accounts

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,8 +1,59 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs")
+const supabase = require("../middleware/supabase.js")
+
+async function insertUser(data) {
+  const { err } = await supabase
+    .from("Users")
+    .insert(data)
+}
+
+async function getUser(email) {
+  const { data, err } = await supabase
+    .from("Users")
+    .select()
+    .eq("email_addr", email)
+
+  return data
+}
+
 
 router.get("/", (req, res) => {
   res.render("login/index");
 });
+
+router.post("/loginAccount", (req, res) => {
+  getUser(req.body.email_addr).then(function(result) {
+    if (!Object.hasOwn(result, 0)) { return }
+
+    bcrypt.compare(req.body.password, result[0].password, (err, equal) => {
+      if (err) {
+        throw err
+      } else {
+        equal ? console.log("correct") : console.log("incorrect")
+      }
+    })
+  })
+
+  res.render("index")
+})
+
+router.post("/createAccount", (req, res) => {
+  getUser(req.body.email_addr).then(function(result) {
+    if (Object.hasOwn(result, 0)) { return }
+
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) {
+        throw err;
+      } else {
+        req.body.password = hash
+        insertUser(req.body)
+      }
+    })
+  })
+
+  res.render("index")
+})
 
 module.exports = router;
