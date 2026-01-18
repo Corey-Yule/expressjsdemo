d8d64731e97cf9d94cf98f402a922b40c2f8c3b4
James <jg2077@hw.ac.uk>
2026-01-18 14:52:26 +0000

    commented the controller file

--- public/javascript/components/controller.js
+++ public/javascript/components/controller.js
@@ -1,3 +1,10 @@
+// This file handles all the importable components
+// when wanting to use a component, just import the relevanta
+// generate function from here and run it on the page
+// that function will import the css and component automatically
+// so that's all that needs to be done
+
+
 import { createBackground } from './background.js'
 import { createNavBar } from './navbar.js'
 
