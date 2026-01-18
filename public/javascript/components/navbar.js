507eadf7760543a9209ab688625899702f386b7e
James <jg2077@hw.ac.uk>
2026-01-18 15:02:16 +0000

    cleanup work

--- public/javascript/components/navbar.js
+++ public/javascript/components/navbar.js
@@ -1,31 +1,3 @@
-// temporarily commented to work on the generation of the nav bar
-//
-// middleware/authContext.js
-// const { checkAuth } = require();
-//
-// module.exports = async function authContext(req, res, next) {
-//   const auth = await checkAuth(req);
-//
-//   res.locals.authenticated = auth.authenticated;
-//   res.locals.user = auth.user;
-//
-//   if (auth.newTokens) {
-//     res.cookie('sb-access-token', auth.newTokens.access_token, {
-//       httpOnly: true,
-//       sameSite: 'lax',
-//       secure: process.env.NODE_ENV === 'production'
-//     });
-//
-//     res.cookie('sb-refresh-token', auth.newTokens.refresh_token, {
-//       httpOnly: true,
-//       sameSite: 'lax',
-//       secure: process.env.NODE_ENV === 'production'
-//     });
-//   }
-//
-//   next();
-// };
-
 const buttons = [{
     anchor: '/account',
     text: 'Account Settings'
@@ -85,3 +57,31 @@ function createNav(container) {
 
   return nav
 }
+
+// temporarily commented to work on the generation of the nav bar
+//
+// middleware/authContext.js
+// const { checkAuth } = require();
+//
+// module.exports = async function authContext(req, res, next) {
+//   const auth = await checkAuth(req);
+//
+//   res.locals.authenticated = auth.authenticated;
+//   res.locals.user = auth.user;
+//
+//   if (auth.newTokens) {
+//     res.cookie('sb-access-token', auth.newTokens.access_token, {
+//       httpOnly: true,
+//       sameSite: 'lax',
+//       secure: process.env.NODE_ENV === 'production'
+//     });
+//
+//     res.cookie('sb-refresh-token', auth.newTokens.refresh_token, {
+//       httpOnly: true,
+//       sameSite: 'lax',
+//       secure: process.env.NODE_ENV === 'production'
+//     });
+//   }
+//
+//   next();
+// };
