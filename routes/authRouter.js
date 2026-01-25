19aec290202807470a262db457bbc38a455e7aae
James <jg2077@hw.ac.uk>
2026-01-25 17:05:10 +0000

    navbar now dynamic based on if the user is logged in

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,19 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const {checkAuth} = require("../middleware/auth.js");
+const { checkAuth } = require("../middleware/auth.js");
 
 router.get("/callback", (req, res) => {
   res.redirect("/dashboard");
 });
 
 router.get("/status", (req, res) => {
-  const token = "sb-access-token";
-  console.log(token)
-  if (token) {
-    return res.json({ loggedIn: true });
-  }
-
-  res.json({ loggedIn: false });
+  checkAuth(req).then(({ authenticated, user }) => {
+    return authenticated ? res.json({ loggedIn: true}) : res.json({ loggedIn: false })
+  })
 });
 
 module.exports = router;
