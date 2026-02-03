5baf4793cc40300eb6fd58bf2272afaf32559609
cy2025 <cy2025@hw.ac.uk>
2026-02-03 17:42:43 +0000

    Signup bug fix

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -101,22 +101,22 @@ router.post("/createAccount", async (req, res) => {
   console.log("Signup successful:", data);
   
   // set cookies for signup (auto-login after signup)
-  if (data.session) {
-    res.cookie('sb-access-token', data.session.access_token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    });
+  // if (data.session) {
+  //   res.cookie('sb-access-token', data.session.access_token, {
+  //     httpOnly: true,
+  //     secure: process.env.NODE_ENV === 'production',
+  //     sameSite: 'lax',
+  //     maxAge: 7 * 24 * 60 * 60 * 1000
+  //   });
 
-    res.cookie('sb-refresh-token', data.session.refresh_token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    });
-  }
-  res.redirect("/"); //dashboard when we actually make it!!!
+  //   res.cookie('sb-refresh-token', data.session.refresh_token, {
+  //     httpOnly: true,
+  //     secure: process.env.NODE_ENV === 'production',
+  //     sameSite: 'lax',
+  //     maxAge: 7 * 24 * 60 * 60 * 1000
+  //   });
+  // }
+  res.redirect("/login"); 
 })
 
 module.exports = router;
